06b8a9465ba5c7dfa988c77639506ec6dc7c6f8b
Izuagbe Samuel. E <izuagbesamuel73@gmail.com>
2024-12-27 02:06:44 +0100

    Ready to fully complete the user management feature

--- src/Infrastructure/Identity/ApplicationUser.cs
+++ src/Infrastructure/Identity/ApplicationUser.cs
@@ -8,9 +8,14 @@ namespace EcommerceAPI.Infrastructure.Identity;
 public class ApplicationUser : IdentityUser
 {
     public string FirstName { get; set; }
+
     public string LastName { get; set; }
+
     public string FullName => $"{FirstName} {LastName}";
+
     public byte[] ProfilePicture { get; set; }
+
     public GenderData Gender { get; set; }
+
     public DateTime LastLoginDate { get; set; }
 }
