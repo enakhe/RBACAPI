1ffa71b7bee92084a3f465b33d745f2a843a0c26
ENAKHE <izuagbesam@gmail.com>
2025-03-31 02:41:02 +0100

    Added a Blazor Pages UI to interact with the web api

--- orchestration/RBACAPI.AppHost/Program.cs
+++ orchestration/RBACAPI.AppHost/Program.cs
@@ -1,5 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Web>("web");
+builder.AddProject<Projects.WebAPI>("web-api");
+
+builder.AddProject<Projects.UI>("ui");
 
 builder.Build().Run();
